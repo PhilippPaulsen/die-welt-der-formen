be98ae67c8f95b6554ab4c54fbebd767b496cad8
Philipp Paulsen <paulsen@phlip.site>
2024-12-29 19:14:54 +0100

    Framework Set!

---
+++ notebooks/click-me.js
@@ -0,0 +1,27 @@
+function _1(md){return(
+md`# Wilhelm Ostwald: 
+# Die Harmonie der Formen`
+)}
+
+function _2(md){return(
+md`This notebook demonstrates the harmony of forms with interactive visualizations.`
+)}
+
+function _counter(html)
+{
+  let value = 0;
+  const button = html`<button>Click me!</button>`;
+  const span = html`<span>${value}</span>`;
+  button.onclick = () => span.textContent = ++value;
+  return html`${button} ${span}`;
+}
+
+
+export default function define(runtime, observer) {
+  const main = runtime.module();
+  main.variable(observer()).define(["md"], _1);
+  main.variable(observer()).define(["md"], _2);
+  main.variable(observer("viewof counter")).define("viewof counter", ["html"], _counter);
+  main.variable(observer("counter")).define("counter", ["Generators", "viewof counter"], (G, _) => G.input(_));
+  return main;
+}
