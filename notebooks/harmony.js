de6abf5715174108a1a48b6f9d6c1fdd22277029
Philipp Paulsen <paulsen@phlip.site>
2024-12-31 18:50:08 +0100

    MD Test

--- notebooks/harmony.js
+++ notebooks/harmony.js
@@ -1,11 +1,10 @@
 function _1(md){return(
-  md`## Wilhelm Ostwald:  
-  ## Die Harmonie der Formen`
+  md` # Die Harmonie der Formen`
   )}
   
   function _2(md){return(
-  md`By Wilhelm Ostwald — With 106 Figures in the Text — Leipzig, 1922 Unesma  
-  Translated and Edited by Philipp Paulsen — Leipzig, 2025 Spector Books`
+  md`By Wilhelm Ostwald, With 106 Figures in the Text, Leipzig, 1922 Unesma  
+  Edited by Philipp Paulsen, Leipzig, 2025 Spector Books`
   )}
   
   function _3(md){return(
@@ -14,7 +13,7 @@ function _1(md){return(
   )}
   
   function _4(md){return(
-  md`### Content
+  md`## Content
   
   - **Chapter 1.** [Space and the Fundamental Law of Spatial Harmony](#space-and-the-fundamental-law-of-spatial-harmony)
     - [Space](#space)
@@ -39,7 +38,7 @@ function _1(md){return(
   
   function _5(md){return(
   md`#### First Chapter
-  ### Space and the Fundamental Principle of Spatial Harmony`
+  ## Space and the Fundamental Principle of Spatial Harmony`
   )}
   
   function _6(md){return(
@@ -53,6 +52,290 @@ function _1(md){return(
   In geometry, the aforementioned determinations are carried out with the utmost rigor. Planes have no thickness, lines no width, and points no extension. The practical-aesthetic spatial theory that concerns us here does not allow for such extensive abstractions. Our lines will always have a distinct width, our planes a certain thickness or height, without letting these considerations disturb us. It is only required that the one-dimensionality in lines and the two-dimensionality in planes predominate so strongly that the existing deviations do not carry weight, i.e., do not significantly alter the phenomena under investigation.`
   )}
   
+  function _8(md){return(
+  md`#### The Uniformity of Space
+  When we move a rigid body through space, we
+  generally perceive it as different. However, through a not entirely
+  simple process of abstraction, we come to the understanding that these
+  differences are merely subjective-perspectival and arise from the
+  changed spatial relationship between the body and the observer's eye,
+  not from the body itself. Instead, we attribute to it everywhere the
+  same shape or spatial nature.  
+  If we successively place the body in adjacent positions, *identical*
+  spatial parts arise according to this premise. This is the foundation of
+  all division and therefore all *measurement* of space. We find that
+  this procedure can be carried out consistently throughout space and
+  therefore attribute to space uniformity (*homogeneity*). `
+  )}
+  
+  function _9(md){return(
+  md`#### Geometric and Illustrative Lines and Points
+  A geometric line, which
+  by definition has no width, is invisible, as visibility requires a width
+  of approximately 0.0001 of the visual angle (i.e., 1 mm at a distance of
+  10 m). Thus, the figures of finite width, to which our considerations
+  apply, are geometrically not lines, but planes, specifically elongated
+  rectangles.  
+  Similarly, geometric points, the only possible distinctions on a
+  geometric line, are invisible and must be replaced by small planes to be
+  visible. `
+  )}
+  
+  function _10(md){return(
+  md`#### The Fundamental Principle of Spatial Harmony
+  If a point is represented by an *arbitrarily* shaped small surface, as shown in Fig.
+  1, we are dissatisfied. We ask the designer why this particular random
+  form was chosen and are only satisfied if an adequate explanation is
+  provided. However, if the point is represented by a circular area, a
+  square, or a hexagon, as in Fig. 2, we agree and raise no further
+  questions. This assumes that the form is not tilted, as in Fig. 3; in
+  such a case, we would inquire about a justification for this
+  arbitrariness, whereas with an upright position, whether horizontal or
+  diagonal, as in Fig. 4, we are satisfied.  
+  Similarly, we agree if a line or stripe is evenly spaced, as shown in
+  Fig. 5. On the other hand, if the division is arbitrary, executed with
+  unequal spacing, as in Fig. 6, we reject it. It lacks the small but
+  distinctly present positive aesthetic value of uniform division, instead
+  possessing a negative one; it appears unattractive.  
+  However, not every unequal division is unattractive. If the spacings of
+  the divisions are assigned as, for example, those on the logarithmic
+  slide rule for equally spaced numbers 1, 2, 3, 4, etc., Fig. 7, a
+  positive aesthetic value arises. These spacings have the property that
+  any two successive ones decrease in the same ratio (according to a
+  geometric or logarithmic series). Upon closer examination, this series
+  even possesses an aesthetic advantage over the evenly spaced one. It
+  appears \\"more interesting\\" because it is much less frequently
+  encountered than the latter.  
+  From these and countless other observations, we derive (in a very broad
+  generalization) a law, which states:  
+  
+  *All lawful forms are harmonious.*  
+  
+  By harmonious, we mean beautiful or pleasant in the broadest sense. This
+  statement, the *Fundamental Principle of Spatial Harmony*, has the
+  nature of a postulate, i.e., a probable assumption that serves as a
+  primary order of facts and whose validity must always be tested through
+  concordance with experience. It is identical to the Fundamental
+  Principle of Color Harmony, and it holds a general significance for all
+  art. We can also express it in the form of an equation:  
+  
+  *Lawfulness = Harmony.*  
+  
+  Goethe conducted his entire artistic life\\'s work in the light of this
+  insight and also expressed it explicitly in the closing words of his art
+  sonnet:  
+  
+  *And only law can give us freedom.*  
+  
+  We therefore call this general law of art, of which color and spatial
+  harmony are merely specific applications, the *Goethean Law*.  `
+  )}
+  
+  function _11(md){return(
+  md`#### Second Chapter  
+  ## Lines`
+  )}
+  
+  function _12(md){return(
+  md`#### Types of Lines
+  The simplest structure that exists here is the
+  uninterrupted, uniformly strong line, as drawn with the ruling pen,
+  pencil, or pen. It can be *straight*, *curved*, or composed of
+  several straight, several curved, or both straight and curved lines. The
+  straight line is the most important because it is the simplest or most
+  lawful case; a completely arbitrary stroke represents the opposite
+  extreme of complete freedom.  
+  The main application of the line is, according to its origin, the
+  delimitation of surfaces. If these are differentiated by color,
+  position, etc., the boundary lines arise naturally. However, art tends
+  to emphasize such natural phenomena with special forms; for example, two
+  different surfaces are usually not simply joined but rather highlighted
+  by a special line (in the graphic sense). Additionally, the line serves
+  as a tool to *divide* a surface that is not yet differentiated, which
+  is a very important task.  `
+  )}
+  
+  function _13(md){return(
+  md`#### Thickness of Lines  
+  The first requirement that must be met by a line is its *clarity*. It
+  was previously noted that the visibility of a line ceases when its width
+  falls below 1/10,000 of the viewing distance. In art, such extreme
+  values (except for special purposes) are widely avoided; a practical
+  lower limit can be set at approximately ten times that value, 1/1,000.
+  For an object with a surface width of 50 cm at its narrowest, the line
+  should not be narrower than 1/2 mm for our purposes.  `
+  )}
+  
+  function _14(md){return(
+  md`#### Edge or Contour
+  As a marker of a boundary, the line must follow it.
+  In the simplest case, it lies directly on the boundary. However, there
+  are numerous cases where it accompanies the boundary at a certain
+  distance. *This distance must be equal everywhere*, measured
+  perpendicular to the boundary line. It must, in any case, be greater
+  than the minimum line width and is usually significantly larger.  
+  For example, if a rectangle is provided with such a boundary line, the
+  distance is not made different along the sides in proportion to the side
+  lengths, Fig. 8, so that the inner rectangle would geometrically
+  resemble the outer one. In such a case, the accompanying line would not
+  be perceived as an *edge*. Instead, the distance is taken to be equal
+  everywhere, Fig. 9, which makes the inner rectangle relatively slimmer
+  than the outer one.  `
+  )}
+  
+  function _15(md){return(
+  md`#### Diversity
+  When going beyond the immediate purpose of marking, one can multiply the simple, uniform line sequence for decorative purposes. This can be done in the following ways:
+  
+  1.  The line is (entirely or approximately) uniform in thickness and continuous;
+  
+  2.  the line is discontinuous, being broken into a series of dots and dashes;
+  
+  3.  the line varies in thickness along its course¹;
+  
+  4.  the line is not straight but dynamic, i.e., somehow curved or broken.  
+      Properties 2, 3, and 4 can simultaneously occur in the same line. 
+      Since each of these properties offers two possibilities (present or
+      not present), there are a total of eight groups encompassing a very
+      large number of individual cases.
+        
+      ¹ H. Prof. Krais brought this diversity to my attention.  `
+  )}
+  
+  function _16(md){return(
+  md`#### Individual Examination  
+  We turn to the separate consideration of the cases just outlined. It
+  should be noted that it will not later be possible to examine all
+  the numerous possibilities arising in the less simple figures
+  (braids, bands, surfaces) with the same level of detail; thus, the
+  following considerations not only serve to elucidate the current
+  task but also as an example of how the reader might later fill in
+  the framework provided by the mathematical treatment of the cases.
+  It should also be noted that the diversities identified here will
+  apply universally to later cases. For we are always dealing either
+  directly with lines or with the mutual boundaries of surfaces, which
+  are also lines. However, in the latter case, one will approach the
+  design of the line in the sense described here with greater restraint.  `
+  )}
+  
+  function _17(md){return(
+  md`#### Discontinuous Lines
+  The resolution of a simple line can occur
+      into *dots* and *dashes*. Accordingly, there are discontinuous
+      lines consisting solely of dots, solely of dashes, and finally of
+      dots alongside dashes.  
+      The dots are either round or square. The second form arises
+      naturally when the line is created with a pen through regular
+      interruptions of the stroke. Round dots are more natural and appear
+      where pen creation is not feasible, for example, in letterpress
+      printing. The spacing, simplest when equal to the dot width, can
+      also be adjusted arbitrarily, Fig. 10 a.  
+      The dashes can vary in length. However, they must not be so short
+      that they are confused with dots, nor so long that the interruptions
+      appear arbitrary. A length five to ten times the thickness seems
+      appropriate. The spacing must maintain a visible proportion to the
+      dash length, approximately 1:5, Fig. 10 b.  
+      It would be worthwhile to examine these proportions more closely to
+      determine where the most pleasing effects occur.  
+      When combining dots and dashes, the alternation *p s p s p s ...* or
+      abbreviated as *p s* is the simplest case, Fig. 10 c. More complex
+      cases, such as *p p s, p p s s, p p p s*, etc., are less common and
+      eventually become confusing.  
+      For all cases considered, the law holds that the sizes and spacings
+      of the segments remain consistent throughout the entire line
+      sequence. An unlawful deviation would appear disorderly and
+      arbitrary, thus unattractive. However, the simple law of equality is
+      not the only possibility. One can also group multiple dots (dashes)
+      into clusters that are separated by larger but still equal intervals
+      among themselves. This creates a more intricate equality, but still
+      maintains lawfulness. `
+  )}
+  
+  function _18(md){return(
+  md`#### Unequal Thickness
+  The design of the writing pen with two
+      springy tips, which open depending on the pressure applied,
+      naturally produces this effect, which is of the most general use in
+      writing. It is noteworthy that the previously (in English script)
+      popular *continuous* transitions from one thickness to another are
+      increasingly regarded as less beautiful and are being replaced by
+      clear distinctions between thick and thin strokes, resulting in a
+      stronger and more \\"characterful\\" script.  
+      This type is used less frequently for line decoration; it is most
+      often employed where a connection to handwritten work is to be
+      emphasized. The diversity is similar to that of the previous case,
+      except that the interruptions of the line are replaced by its
+      variations in thickness.`
+  )}
+  
+  function _19(md){return(
+  md`#### Dynamic Lines
+  As book ornaments, lines can be found that show
+      small irregularities, as if drawn freehand. They are an expression
+      of a skewed sentimentality that ascribes a \\"higher artistic value\\"
+      to craftsmanship and seeks to create its semblance even where it is
+      evidently excluded by the method of production.  
+      Significantly different from these are the *lawful* dynamics. As a
+      very general principle, *repetition* emerges, as already discussed
+      in the context of interrupted lines. Through lawful repetition, one
+      can remove arbitrariness from any free stroke and bestow upon it the
+      simple aesthetic value inherent in visual lawfulness.  `
+  )}
+  
+  function _20(md){return(
+  md`#### Shifting.
+  Repetition occurs along the direction of the entire
+      line, as the chosen form is repeated at a fixed, consistent
+      interval, effectively "shifted." This process is thus referred to
+      as *shifting*.  
+      Shifting involves mapping each point in the original form to a
+      corresponding point in the second position, equidistant from the
+      original. All these connecting lines, of identical length, are also
+      *parallel*¹.  
+      This results in an exact repetition of the original form at a
+      defined distance from it.  
+      Shifting is one of the three fundamental operations through which a
+      simple form element can give rise to a lawfully composed form. For
+      this reason, it will frequently be employed in the discussions that
+      follow.  
+      Shifting can be repeated indefinitely, producing lines of any
+      desired length.`
+  )}
+  
+  function _21(md){return(
+  md`1) For the three-syllable foreign word *parallel*, Ostwald's original German text uses the term *bar* employed by Albrecht Dürer.`
+  )}
+  
+  function _22(md){return(
+  md`#### Sequences and Waves.
+  The line repeated through shifting is
+      initially assumed to be entirely arbitrary. In particular, we do not
+      assume that the beginning and end of the repetitions of the original
+      form created by shifting coincide. The only regularity present in
+      the resulting overall form, which we shall call a *sequence*, is
+      limited to the bare repetition. Fig. 11 shows a sequence.  
+      *Fig. 11*  
+      If the form is such that the beginning aligns with the end during
+      repetition, resulting in a single, continuous line as the long form
+      develops, the resulting structure is called a *wave*.  
+      We initially apply no further rules to the wave other than that the
+      line runs continuously, Fig. 12.  
+      *Fig. 12*  
+      The size of the shift for any form to create waves is called the
+      *wavelength*. Since, according to the description of shifting, all
+      distances between corresponding points are equal, the same
+      wavelength is found regardless of the point from which it is
+      measured to the corresponding point of repetition. The *wave* can
+      therefore be viewed and drawn from any arbitrary point. Often, the
+      original form is such that it contains particularly distinctive
+      points, which are well-suited for measurement.  
+      Every wave has a specific *wave height*. Through the highest and
+      lowest points of the wave, one draws a straight line in the
+      direction of the wave; the distance between these lines is the wave
+      height. This determines the width of the strip covered by the
+      wave.  `
+  )}
+  
   export default function define(runtime, observer) {
     const main = runtime.module();
     main.variable(observer()).define(["md"], _1);
@@ -62,6 +345,21 @@ function _1(md){return(
     main.variable(observer()).define(["md"], _5);
     main.variable(observer()).define(["md"], _6);
     main.variable(observer()).define(["md"], _7);
+    main.variable(observer()).define(["md"], _8);
+    main.variable(observer()).define(["md"], _9);
+    main.variable(observer()).define(["md"], _10);
+    main.variable(observer()).define(["md"], _11);
+    main.variable(observer()).define(["md"], _12);
+    main.variable(observer()).define(["md"], _13);
+    main.variable(observer()).define(["md"], _14);
+    main.variable(observer()).define(["md"], _15);
+    main.variable(observer()).define(["md"], _16);
+    main.variable(observer()).define(["md"], _17);
+    main.variable(observer()).define(["md"], _18);
+    main.variable(observer()).define(["md"], _19);
+    main.variable(observer()).define(["md"], _20);
+    main.variable(observer()).define(["md"], _21);
+    main.variable(observer()).define(["md"], _22);
     return main;
   }
   
